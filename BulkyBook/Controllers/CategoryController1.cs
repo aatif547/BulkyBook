5980bb1da9393f5c8849a87a3c5f2e5f58b9c627
aatif547 <aatifsarfaraz547@gmail.com>
2022-10-08 20:56:15 +0530

    Display Category

--- BulkyBook/Controllers/CategoryController1.cs
+++ BulkyBook/Controllers/CategoryController1.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBook.Data;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBook.Controllers
 {
     public class CategoryController1 : Controller
     {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController1(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+
+            IEnumerable<Category> objCategoryList= _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
